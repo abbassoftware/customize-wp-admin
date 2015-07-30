973663a7d6e250c99724bdb3406ee84e8b3400b6
Abbas Suterwala <abbas.s786@yahoo.co.in>
2015-07-30 18:32:22 +0530

    Made changes to customize the admin menu

--- customize-wp-admin.php
+++ customize-wp-admin.php
@@ -7,12 +7,12 @@
  */
 function change_login_logo() {
     ?>
-    <style type="text/css">
-        .login h1 a {
-        	background-image: url(<?php echo plugins_url ( 'images/mylogo.png', __FILE__ );
-            ?>);
-    }
-    </style>
+<style type="text/css">
+.login h1 a {
+	background-image: url(<?php echo plugins_url ( 'images/mylogo.png', __FILE__);
+	?>);
+}
+</style>
 <?php
 }
 add_action ( 'login_enqueue_scripts', 'change_login_logo' );
@@ -33,3 +33,26 @@ function custom_widget_details() {
  echo 'I can add any php or HTML code here to provide any functionality you like.';
 }
 add_action( 'wp_dashboard_setup', 'add_custom_admin_widgets' );
+
+function remove_menu_items() {
+    remove_menu_page ( 'tools.php' );
+}
+add_action ( 'admin_menu', 'remove_menu_items' );
+
+
+
+
+function add_menu_item() {
+    add_menu_page( 'My menu item', 'My menu item', 'manage_options', 'my-menu-item-unique-id', 'menu_item_details', '' , 0 );
+}
+
+add_action( 'admin_menu', 'add_menu_item' );
+
+function menu_item_details() {
+    echo '<div class="wrap">';
+    echo '<p>';
+    echo 'This is a custom Menu.<br>';
+    echo 'I can add any php or HTML code here to provide any functionality you like.';
+    echo '</p>';
+    echo '</div>';
+}
