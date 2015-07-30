b6c0c1362dd79b15f81fdc8d10807f939abe9d17
Abbas Suterwala <abbas.s786@yahoo.co.in>
2015-07-30 17:30:55 +0530

    Made changes to customize the admin dashboard

--- customize-wp-admin.php
+++ customize-wp-admin.php
@@ -1,16 +1,35 @@
 <?php
 /*
- Plugin Name: Customize wp-admin
- Description: This plugin shows you how to customize your WordPress Admin section.
- Author: Abbas Suterwala.
- Version: 1.0
+ * Plugin Name: Customize wp-admin
+ * Description: This plugin shows you how to customize your WordPress Admin section.
+ * Author: Abbas Suterwala.
+ * Version: 1.0
  */
-
-function change_login_logo() { ?>
+function change_login_logo() {
+    ?>
     <style type="text/css">
         .login h1 a {
-            background-image: url(<?php echo plugins_url( 'images/mylogo.png', __FILE__ ); ?>);
-        }
+        	background-image: url(<?php echo plugins_url ( 'images/mylogo.png', __FILE__ );
+            ?>);
+    }
     </style>
-<?php }
-add_action( 'login_enqueue_scripts', 'change_login_logo' );
+<?php
+}
+add_action ( 'login_enqueue_scripts', 'change_login_logo' );
+
+function remove_admin_widgets() {
+    remove_meta_box ( 'dashboard_quick_press', 'dashboard', 'side' );
+    remove_meta_box ( 'dashboard_primary', 'dashboard', 'side' );
+}
+add_action ( 'wp_dashboard_setup', 'remove_admin_widgets' );
+
+
+function add_custom_admin_widgets() {
+    wp_add_dashboard_widget( 'my_dashboard_custom_widget', 'Custom Widget', 'custom_widget_details' );
+}
+
+function custom_widget_details() {
+ echo 'This is a custom widget.<br>';
+ echo 'I can add any php or HTML code here to provide any functionality you like.';
+}
+add_action( 'wp_dashboard_setup', 'add_custom_admin_widgets' );
